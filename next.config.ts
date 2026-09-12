d637eb5699e1da7a0cc36cc444d8fc5fa6170b22
webdevpemula <reynatafachrian7@gmail.com>
2026-09-12 09:09:17 +0700

    fix(security): update header route wildcard to /:path*

--- next.config.ts
+++ next.config.ts
@@ -33,7 +33,7 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: '/(.*)',
+        source: '/:path*',
         headers: securityHeaders,
       },
     ];
